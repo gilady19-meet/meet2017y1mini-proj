cc5f5a4878bc89f85228bba93d7de45bf1fe8639
gilady19-meet <gilady19@meet.mit.edu>
2017-08-02 10:27:47 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -19,6 +19,11 @@ food_stamps = []
 snake = turtle.clone()
 snake.shape("square")
 
+turtle.register_shape("trash.gif")
+
+food = turtle.clone()
+food.shape("trash.gif")
+
 turtle.hideturtle()
 
 for i in range(START_LENGTH):
@@ -82,7 +87,24 @@ turtle.onkeypress(right, RIGHT_ARROW)
 
 turtle.listen()
 
+def make_food():
+    min_x=-int(SIZE_X/2/SQUARE_SIZE)+1
+    max_x=int(SIZE_X/2/SQUARE_SIZE)-1
+    min_y=-int(SIZE_Y/2/SQUARE_SIZE)+1
+    max_y=int(SIZE_Y/2/SQUARE_SIZE)-1
+
+    food_x = (random.randint(min_x,max_x))*SQUARE_SIZE
+    food_y = (random.randint(min_y,max_y))*SQUARE_SIZE
+
+    food.goto(food_x, food_y)
+    food_pos.append((food_x, food_y))
+    c = food.stamp()
+    food_stamps.append(c)
+
+
 def move_snake():
+
+
     my_pos = snake.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
@@ -104,9 +126,20 @@ def move_snake():
     pos_list.append(my_pos)
     new_stamp = snake.stamp()
     stamp_list.append(new_stamp)
-    #part5
     old_stamp = stamp_list.pop(0)
     snake.clearstamp(old_stamp)
+
+    # here
+    global food_stamps, food_pos
+    if snake.pos() in food_pos:
+        food_ind=food_pos.index(snake.pos())
+        food.clearstamp(food_stamps[food_ind])
+        food_pos.pop(food_ind)
+        food_stamps.pop(food_ind)
+        print("You have eaten the food!")
+        make_food()
+        #part 8
+
     pos_list.pop(0)
 
     new_pos = snake.pos()
@@ -128,11 +161,15 @@ def move_snake():
     turtle.ontimer(move_snake,TIME_STEP)
 
 move_snake()
+make_food()
 
-turtle.register_shape("trash.gif")
 
-food = turtle.clone()
-food.shape("trash.gif")
 
-food_pos = [(100,100) , (-100,100) , (-100,-100) , (100,-100)]
-food_stamps = []
+#food_pos = [(100,100) , (-100,100) , (-100,-100) , (100,-100)]
+
+#for this_food_pos in food_pos :
+    
+    #food.hide()
+    
+
+    
